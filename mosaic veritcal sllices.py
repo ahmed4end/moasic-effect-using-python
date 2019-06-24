2a6eb0a575c7b62aa4064757259b0b75ab2076c6
as4end <35382418+as4end@users.noreply.github.com>
2019-06-24 14:25:13 +0200

    Update mosaic veritcal sllices.py

--- mosaic veritcal sllices.py
+++ mosaic veritcal sllices.py
@@ -8,15 +8,13 @@ Image.MAX_IMAGE_PIXELS = 100000000  # For PIL Image error when handling very lar
 os.chdir("./data/output/")
 
 
-#imgs = sorted(glob.glob("*.jpg"))[:50]
-
 imgs = glob.glob("*.jpg")
 
 concatenated = Image.fromarray(
   np.concatenate(
-    [np.array(Image.open(x)) for x in imgs],
+    [np.array(Image.open(x)) for x in [str(i)+".jpg" for i in range(1,len(imgs))]],
     axis=0
   )
 )
 
-concatenated.save("finale.jpg")
+concatenated.save("output.jpg")
